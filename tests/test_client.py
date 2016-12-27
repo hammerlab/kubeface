30e222af6c0573e1c9106746d532ed678cfe4236
Tim O'Donnell <timodonnell@gmail.com>
2016-12-27 16:02:16 -0500

    fixes

--- tests/test_client.py
+++ tests/test_client.py
@@ -5,6 +5,7 @@ from kubeface import (
     client,
     local_process_backend,
     local_process_docker_backend,
+    worker_configuration,
     common)
 
 from .util import with_bucket
@@ -25,16 +26,22 @@ def test_local_process_backend(bucket):
 
 @with_bucket
 def test_local_process_docker_backend(bucket):
-    backend = local_process_docker_backend.LocalProcessDockerBackend()
+    branch = "master"
+    branch = "experiments"
+    worker_config = worker_configuration.WorkerConfiguration(
+        kubeface_install_command=(
+        "{pip} install https://github.com/hammerlab/kubeface/archive/%s.zip" % branch))
+    backend = local_process_docker_backend.LocalProcessDockerBackend(
+        worker_configuration=worker_config)
     c = client.Client(
         backend,
         poll_seconds=1.0,
-        max_simultaneous_tasks=3,
+        max_simultaneous_tasks=1,
         storage_prefix=bucket)
-    exercise_client(c)
+    exercise_client(c, num=3)
 
 
-def exercise_client(c):
+def exercise_client(c, num=10):
     testing.assert_equal(
-        list(c.map(lambda x: x**2, range(10))),
-        numpy.arange(10)**2)
+        list(c.map(lambda x: x**2, range(num))),
+        numpy.arange(num)**2)
