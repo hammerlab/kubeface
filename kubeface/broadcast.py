fc7d5f010348c5e12c771cf3e1d4d72ebd490e1d
Julia K <julia326@gmail.com>
2017-01-11 18:20:22 -0500

    cr tim

--- kubeface/broadcast.py
+++ kubeface/broadcast.py
@@ -3,6 +3,9 @@ import tempfile
 
 from . import common, naming, serialization, storage
 
+
+_KEY = 'filename'
+
 class Broadcast(object):
 
     def __init__(self, storage_prefix, cache_key_prefix, data):
@@ -13,19 +16,22 @@ class Broadcast(object):
     def compute_filename(self):
         return naming.make_broadcast_name(self.cache_key_prefix)
 
-    """ Returns the path to which the data was written. """
+    """
+    Returns a dict with one key-value pair, where the value is the path to which the data was
+    written.
+    """
     def __getstate__(self):
         # load data into a local tempfile and write out to storage
-        hash_path = '%s/%s' % (self.storage_prefix, self.compute_filename())
+        filename = '%s/%s' % (self.storage_prefix, self.compute_filename())
         with tempfile.TemporaryFile(prefix="broadcast-data-") as fd:
             serialization.dump(self.data, fd)
             size_string = common.human_readable_memory_size(fd.tell())
-            logging.info("Writing broadcast data (%s) to path %s", size_string, hash_path)
+            logging.info("Writing broadcast data (%s) to path %s", size_string, filename)
             fd.seek(0)
-            storage.put(hash_path, fd)
+            storage.put(filename, fd)
 
-        return hash_path
+        return {_KEY: filename}
 
     def __setstate__(self, state):
-        input_handle = storage.get(state)
+        input_handle = storage.get(state[_KEY])
         self.data = serialization.load(input_handle)
